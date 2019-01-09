e61d49e1023c80125468ebbec94fb5f86fd571b6
Aashish Dugar <31813549+aashishdugar@users.noreply.github.com>
2019-01-09 09:51:03 -0500

    Add files via upload

---
+++ arima.py
@@ -0,0 +1,63 @@
+from pandas import read_csv
+from pandas import DataFrame
+from statsmodels.tsa.arima_model import ARIMA
+from pandas.tools.plotting import autocorrelation_plot
+from matplotlib import pyplot
+from statsmodels.graphics.tsaplots import plot_pacf
+
+
+#DIRECTORY
+#series = read_csv('C:/Users/aashi/Desktop/DataSet/Segment5/trainingdatatimeformat.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+"""#plot_pacf(series, lags=50)
+autocorrelation_plot(series)
+#series.plot()
+pyplot.show()"""
+
+
+# fit model
+"""model = ARIMA(series, order=(10, 1, 0))
+model_fit = model.fit(disp=0)
+print(model_fit.summary())
+# plot residual errors
+residuals = DataFrame(model_fit.resid)
+residuals.plot()
+pyplot.show()
+residuals.plot(kind='kde')
+print("mean is: ")
+print(residuals.mean())
+pyplot.show()
+print(residuals.describe())"""
+
+
+
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
+
+#DIRECTORY
+series1 = read_csv('C:/Users/aashi/Desktop/DataSet/Segment5/trainingdatatimeformat.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+series2 = read_csv('C:/Users/aashi/Desktop/DataSet/Segment5/testdata.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+#date_parser=parser)
+X = series1.values
+Y = series2.values
+#size = int(len(X) * 0.66)
+train, test = X[0:int(len(Y))], Y[0:int(len(Y))]
+history = [x for x in train]
+predictions = list()
+for t in range(len(test)):
+    model = ARIMA(history, order=(2, 1, 0))
+    model_fit = model.fit(disp=0)
+    output = model_fit.forecast()
+    yhat = output[0]
+    predictions.append(yhat)
+    obs = test[t]
+    history.append(obs)
+    print('predicted=%f, expected=%f' % (yhat, obs))
+error = mean_squared_error(test, predictions)
+print('Test MSE: %.3f' % error)
+# plot
+pyplot.plot(test)
+pyplot.plot(predictions, color='red')
+pyplot.show()
